2ee37a4582c2be36df2bd3b32427bced47950319
Sijan Malla <sijan.malla101@gmail.com>
2023-09-21 16:53:59 -0500

    Added Newsletter form and Action API

--- src/pages/NewsLetter.jsx
+++ src/pages/NewsLetter.jsx
@@ -1,7 +1,71 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+import { Form, redirect } from 'react-router-dom';
+import axios from 'axios';
+
+const newsletterUrl = 'https://www.course-api.com/cocktails-newsletter';
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const response = await axios.post(newsletterUrl, data);
+  toast.success(response.data.msg);
+  return redirect('/');
+};
 
 const NewsLetter = () => {
-  return <div>NewsLetter</div>;
+  return (
+    <Form className="form" method="POST">
+      <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        Our Newsletter
+      </h4>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="form-input"
+          required
+        />
+      </div>
+      {/* lastname */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          lastName
+        </label>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          className="form-input"
+          required
+        />
+      </div>
+      {/* email */}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="form-input"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: '0.5rem' }}
+      >
+        submit
+      </button>
+    </Form>
+  );
 };
 
 export default NewsLetter;
